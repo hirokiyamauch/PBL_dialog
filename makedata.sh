e7ef8eb58f1e8327896dba558c11a094312e8ec5
hirokiyamauchi <93113466+hirokiyamauch@users.noreply.github.com>
2022-11-05 12:30:59 +0900

    Delete makedata.sh

--- makedata.sh
+++
@@ -1,6 +0,0 @@
-#LOAD_FILEとSAVE_DIRを指定#
-LOAD_FILE="../data/orig/tweet_pairs.txt"
-SAVE_DIR="../data/OpenNMT"
-
-mkdir -p $SAVE_DIR
-python python/generate_data_for_opennmt.py -lf $LOAD_FILE -sd $SAVE_DIR
