89a6b681adce050c387349a31ae3e05a55f3264d
hirokiyamauchi <93113466+hirokiyamauch@users.noreply.github.com>
2022-11-05 12:41:58 +0900

    Update translate.sh

--- translate.sh
+++ translate.sh
@@ -1,3 +1,3 @@
 #model モデルの保存場所 src testデータの場所 output 出力ファイル
 mkdir -p ../../result
-python ../../download/OpenNMT-py/translate.py -model "../../model/dlg_model" -src ../../data/OpneNMT/test.src -output ../../result/pred.txt -replace_unk -verbose
+python ../../download/OpenNMT-py/translate.py -model "../../model/dlg_model" -src ../../data/OpenNMT/test.src -output ../../result/pred.txt -replace_unk -verbose
